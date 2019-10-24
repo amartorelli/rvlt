ef8e3cfaa2b76635b820f3cace489a5aaa2696f6
Alessio Martorelli <alessio.martorelli@gmail.com>
2019-10-24 11:35:25 +0100

    Added deployment script - renamed helm chart folder

---
+++ deploy/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Pre-requisite: helm plugin install https://github.com/ContainerSolutions/helm-monitor
+
+helm upgrade -i -f values.yaml rvlt-charts/helloworld --version $HELLOWORLD_VER helloworld
+
+# Automatic roll back if 5xx are returned
+
+helm monitor prometheus --prometheus=http://prometheus helloworld 'rate(helloworld_http_requests_total{code=~"^5.*$"}[5m]) > 0'
